e62dd9d848e5f09288fc89d4ae70232fcea1fb46
Armin Häberling <armin.aha@gmail.com>
2024-09-11 14:19:50 +0200

    Refactoring: Move handleOffer from ShoppingCart to Teller

--- kotlin/src/main/kotlin/dojo/supermarket/model/ShoppingCart.kt
+++ kotlin/src/main/kotlin/dojo/supermarket/model/ShoppingCart.kt
@@ -18,55 +18,7 @@ class ShoppingCart {
         items.add(ProductQuantity(product, quantity))
     }
 
-    fun handleOffers(offers: Map<Product, Offer>, catalog: SupermarketCatalog) : List<Discount> {
-        val discounts : MutableList<Discount> = mutableListOf()
-        for (productQuantity in productQuantities()) {
-            val product = productQuantity.key
-            val quantity = productQuantity.value
-            if (offers.containsKey(product)) {
-                val offer = offers[product]!!
-                val unitPrice = catalog.getUnitPrice(product)
-                val quantityAsInt = quantity.toInt()
-                var discount: Discount? = null
-                var x = 1
-                if (offer.offerType === SpecialOfferType.ThreeForTwo) {
-                    x = 3
-
-                } else if (offer.offerType === SpecialOfferType.TwoForAmount) {
-                    x = 2
-                    if (quantityAsInt >= 2) {
-                        val total = offer.argument * (quantityAsInt / x) + quantityAsInt % 2 * unitPrice
-                        val discountN = unitPrice * quantity - total
-                        discount = Discount(product, "2 for " + offer.argument, discountN)
-                    }
-
-                }
-                if (offer.offerType === SpecialOfferType.FiveForAmount) {
-                    x = 5
-                }
-                val numberOfXs = quantityAsInt / x
-                if (offer.offerType === SpecialOfferType.ThreeForTwo && quantityAsInt > 2) {
-                    val discountAmount =
-                        quantity * unitPrice - (numberOfXs.toDouble() * 2.0 * unitPrice + quantityAsInt % 3 * unitPrice)
-                    discount = Discount(product, "3 for 2", discountAmount)
-                }
-                if (offer.offerType === SpecialOfferType.TenPercentDiscount) {
-                    discount =
-                        Discount(product, offer.argument.toString() + "% off", quantity * unitPrice * offer.argument / 100.0)
-                }
-                if (offer.offerType === SpecialOfferType.FiveForAmount && quantityAsInt >= 5) {
-                    val discountTotal =
-                        unitPrice * quantity - (offer.argument * numberOfXs + quantityAsInt % 5 * unitPrice)
-                    discount = Discount(product, x.toString() + " for " + offer.argument, discountTotal)
-                }
-                if (discount != null)
-                    discounts.add(discount)
-            }
-        }
-        return discounts
-    }
-
-    private fun productQuantities(): Map<Product, Double> {
+    fun productQuantities(): Map<Product, Double> {
         // TODO simplify!
         return items.groupBy { it.product }
             .map { entry -> entry.key to entry.value.sumOf { it.quantity } }
--- kotlin/src/main/kotlin/dojo/supermarket/model/Teller.kt
+++ kotlin/src/main/kotlin/dojo/supermarket/model/Teller.kt
@@ -16,8 +16,67 @@ class Teller(private val catalog: SupermarketCatalog) {
                 val unitPrice = this.catalog.getUnitPrice(it.product)
                 ReceiptItem(it.product, it.quantity, unitPrice)
             }
-        val discounts = shoppingCart.handleOffers(this.offers, this.catalog)
+        val discounts = handleOfferShoppingCart(shoppingCart = shoppingCart)
         return Receipt(receiptItemList = productQuantities, discountList = discounts)
     }
+
+    private fun handleOfferShoppingCart(shoppingCart: ShoppingCart): List<Discount> {
+        return shoppingCart.productQuantities()
+            .map {
+                handleOfferProduct(it.key, it.value)
+            }.filterNotNull()
+    }
+
+    private fun handleOfferProduct(
+        product: Product,
+        quantity: Double
+    ): Discount? {
+        val offer = offers[product] ?: return null
+        val unitPrice = catalog.getUnitPrice(product)
+        return discount(quantity, offer, unitPrice, product)
+    }
+
+    private fun discount(
+        quantity: Double,
+        offer: Offer,
+        unitPrice: Double,
+        product: Product
+    ): Discount? {
+        val quantityAsInt = quantity.toInt()
+        var discount: Discount? = null
+        var x = 1
+        if (offer.offerType === SpecialOfferType.ThreeForTwo) {
+            x = 3
+
+        } else if (offer.offerType === SpecialOfferType.TwoForAmount) {
+            x = 2
+            if (quantityAsInt >= 2) {
+                val total = offer.argument * (quantityAsInt / x) + quantityAsInt % 2 * unitPrice
+                val discountN = unitPrice * quantity - total
+                discount = Discount(product, "2 for " + offer.argument, discountN)
+            }
+
+        }
+        if (offer.offerType === SpecialOfferType.FiveForAmount) {
+            x = 5
+        }
+        val numberOfXs = quantityAsInt / x
+        if (offer.offerType === SpecialOfferType.ThreeForTwo && quantityAsInt > 2) {
+            val discountAmount =
+                quantity * unitPrice - (numberOfXs.toDouble() * 2.0 * unitPrice + quantityAsInt % 3 * unitPrice)
+            discount = Discount(product, "3 for 2", discountAmount)
+        }
+        if (offer.offerType === SpecialOfferType.TenPercentDiscount) {
+            discount =
+                Discount(product, offer.argument.toString() + "% off", quantity * unitPrice * offer.argument / 100.0)
+        }
+        if (offer.offerType === SpecialOfferType.FiveForAmount && quantityAsInt >= 5) {
+            val discountTotal =
+                unitPrice * quantity - (offer.argument * numberOfXs + quantityAsInt % 5 * unitPrice)
+            discount = Discount(product, x.toString() + " for " + offer.argument, discountTotal)
+        }
+        return discount
+    }
+
 
 }
